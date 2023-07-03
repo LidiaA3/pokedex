0d80337dfdad56c07efbbabe01d6ca2b189be4de
Lidia A <lidia.lasy@gmail.com>
2023-07-03 16:19:14 +0200

    Add view render test

---
+++ tests/renderViews.test.jsx
@@ -0,0 +1,22 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react'
+
+import Home from './src/views/home/Home';
+import Favourites from '../src/views/favourites/Favourites';
+import Detail from '../src/views/detail/Detail';
+
+describe('Views render property', () => {
+    it('The home view renders appropriately', () => {
+        render(<Home />)
+        expect(screen.getByText(/prev/i)).toBeInTheDocument()
+        expect(screen.getByText(/next/i)).toBeInTheDocument()
+      })
+      it('The detail view renders appropriately', () => {
+        render(<Detail />)
+        expect(screen.getByText(/Loading/i)).toBeInTheDocument()
+      })
+      it('The favourite view renders appropriately', () => {
+        render(<Favourites />)
+        expect(screen.getByText(/Favourite pokemons/i)).toBeInTheDocument()
+      })
+});
